35f348d4ea55230b5e8cb67b284232681ddba7ea
slborisova <slborisova@gmail.com>
2024-11-03 10:54:10 -0500

    Constant name and locator changed

--- tests/inputFields.spec.ts
+++ tests/inputFields.spec.ts
@@ -8,7 +8,7 @@ test.beforeEach(async ({ page }) => {
 
 test.describe("Interacting with Input Fields", async () => {
   test("Update pet type", async ({ page }) => {
-    const editPetType = page.getByRole("textbox");
+    const editPetTypeInputField = page.getByRole("textbox");
     const editButton = page.getByRole("button", { name: "Edit" });
     const updateButton = page.getByRole("button", { name: "Update" });
 
@@ -16,32 +16,32 @@ test.describe("Interacting with Input Fields", async () => {
 
     await expect(page.locator("h2")).toHaveText("Edit Pet Type");
 
-    await editPetType.click();
-    await editPetType.clear();
-    await editPetType.fill("rabbit");
+    await editPetTypeInputField.click();
+    await editPetTypeInputField.clear();
+    await editPetTypeInputField.fill("rabbit");
     await updateButton.click();
 
     await expect(page.locator('[id="0"]')).toHaveValue("rabbit");
 
     await editButton.first().click();
 
-    await editPetType.click();
-    await editPetType.clear();
-    await editPetType.fill("cat");
+    await editPetTypeInputField.click();
+    await editPetTypeInputField.clear();
+    await editPetTypeInputField.fill("cat");
     await updateButton.click();
 
     await expect(page.locator('[id="0"]')).toHaveValue("cat");
   });
 
   test("Cancel pet type update", async ({ page }) => {
-    const editPetType = page.getByRole("textbox");
+    const editPetTypeInputField = page.getByRole("textbox");
 
     await page.getByRole("button", { name: "Edit" }).nth(1).click();
-    await editPetType.click();
-    await editPetType.clear();
-    await editPetType.fill("moose");
+    await editPetTypeInputField.click();
+    await editPetTypeInputField.clear();
+    await editPetTypeInputField.fill("moose");
 
-    await expect(editPetType).toHaveValue("moose");
+    await expect(editPetTypeInputField).toHaveValue("moose");
 
     await page.getByRole("button", { name: "Cancel" }).click();
 
@@ -49,14 +49,14 @@ test.describe("Interacting with Input Fields", async () => {
   });
 
   test("Pet type name is required validation", async ({ page }) => {
-    const editPetType = page.getByRole("textbox");
+    const editPetTypeInputField = page.getByRole("textbox");
 
     await page.getByRole("button", { name: "Edit" }).nth(2).click();
 
-    await editPetType.click();
-    await editPetType.clear();
+    await editPetTypeInputField.click();
+    await editPetTypeInputField.clear();
 
-    await expect(page.locator('[class="help-block"]')).toHaveText( "Name is required");
+    await expect(page.locator('.help-block')).toHaveText( "Name is required");
 
     await page.getByRole("button", { name: "Update" }).click();
 
