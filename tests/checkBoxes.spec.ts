c3dbd79e5be00d49be6372b17b28a3ca72bf5d31
slborisova <slborisova@gmail.com>
2024-11-04 23:58:29 -0500

    CheckBoxes homework

---
+++ tests/checkBoxes.spec.ts
@@ -0,0 +1,70 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+  await page.getByText("Veterinarians").click();
+  await page.getByText("All").click();
+  await expect(page.locator("h2")).toHaveText("Veterinarians");
+});
+
+test.describe("Check Boxes", async () => {
+  test("Validate selected specialties", async ({ page }) => {
+    const specialtiesInputField = page.locator(".selected-specialties");
+
+    await page.getByRole("button", { name: "Edit Vet" }).nth(1).click();
+
+    await expect(specialtiesInputField).toHaveText("radiology");
+
+    await page.locator(".dropdown-arrow").click();
+
+    expect(await page.getByRole("checkbox", { name: "radiology" }).isChecked()).toBeTruthy();
+
+    expect(await page.getByRole("checkbox", { name: "surgery" }).isChecked()).toBeFalsy();
+    expect(await page.getByRole("checkbox", { name: "dentistry" }).isChecked()).toBeFalsy();
+
+    await page.getByRole("checkbox", { name: "surgery" }).check({ force: true });
+    await page.getByRole("checkbox", { name: "radiology" }).uncheck({ force: true });
+
+    await expect(specialtiesInputField).toHaveText("surgery");
+
+    await page.getByRole("checkbox", { name: "dentistry" }).check({ force: true });
+
+    await expect(specialtiesInputField).toHaveText("surgery, dentistry");
+  });
+
+  test("Select all specialties", async ({ page }) => {
+    const specialtiesInputField = page.locator(".selected-specialties");
+
+    await page.getByRole("button", { name: "Edit Vet" }).nth(3).click();
+
+    await expect(specialtiesInputField).toHaveText("surgery");
+
+    await page.locator(".dropdown-arrow").click();
+
+    const allBoxes = page.getByRole("checkbox");
+    for (const box of await allBoxes.all()){
+        await box.check({ force: true });
+        expect(await box.isChecked()).toBeTruthy();
+    }
+    await expect(specialtiesInputField).toHaveText(
+      "surgery, radiology, dentistry, New specialty"
+    );
+  });
+
+  test("Unselect all specialties", async ({ page }) => {
+    const specialtiesInputField = page.locator(".selected-specialties");
+
+    await page.getByRole("button", { name: "Edit Vet" }).nth(2).click();
+
+    await expect(specialtiesInputField).toHaveText("dentistry, surgery");
+
+    await page.locator(".dropdown-arrow").click();
+
+    const allBoxes = page.getByRole("checkbox");
+    for (const box of await allBoxes.all()){
+      await box.uncheck({ force: true });
+      expect(await box.isChecked()).toBeFalsy();
+    }
+    await expect(specialtiesInputField).toBeEmpty();
+  });
+});
