3a8994500234010326f3c1266270fa540d57d4ef
slborisova <slborisova@gmail.com>
2024-11-14 10:51:03 -0500

    DialogBoxes Homework

---
+++ tests/dialogBoxes.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+  await page.getByTitle("pettypes").click();
+  await expect(page.locator("h2")).toHaveText("Pet Types");
+});
+
+test.describe("Dialog Boxes", async () => {
+  test("Add and delete pet type", async ({ page }) => {
+    await page.getByRole("button", { name: "Add" }).click();
+
+    const nameInputField = page.locator("#name");
+    await expect(nameInputField).toBeVisible();
+
+    await nameInputField.fill("pig");
+    await page.getByRole("button", { name: "Save" }).click();
+
+    await expect(page.locator("table tr td input").last()).toHaveValue("pig");
+
+    page.on("dialog", (dialog) => {
+      expect(dialog.message()).toEqual("Delete the pet type?");
+      dialog.accept();
+    });
+
+    await page
+      .locator("table tr")
+      .last()
+      .getByRole("button", { name: "Delete" })
+      .click();
+    await expect(page.locator("table tr").last()).not.toHaveText("pig");
+  });
+});
