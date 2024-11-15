b71a4fcc46a2e6e3b28d53d9a5547a02c69a3ac6
slborisova <slborisova@gmail.com>
2024-11-14 19:17:13 -0500

    DialogBoxes Homework fixed 4

--- tests/dialogBoxes.spec.ts
+++ tests/dialogBoxes.spec.ts
@@ -16,8 +16,8 @@ test.describe("Dialog Boxes", async () => {
     await nameInputField.fill("pig");
     await page.getByRole("button", { name: "Save" }).click();
 
-    const petTypesTableInputFields = page.locator("table tr td input").last();
-    await expect(petTypesTableInputFields).toHaveValue("pig");
+    const petTypesTableLastInputField = page.locator("table tr td input").last();
+    await expect(petTypesTableLastInputField).toHaveValue("pig");
 
     page.on("dialog", (dialog) => {
       expect(dialog.message()).toEqual("Delete the pet type?");
@@ -29,6 +29,6 @@ test.describe("Dialog Boxes", async () => {
       .last()
       .getByRole("button", { name: "Delete" })
       .click();
-    await expect(petTypesTableInputFields).not.toHaveValue("pig");
+    await expect(petTypesTableLastInputField).not.toHaveValue("pig");
   });
 });
