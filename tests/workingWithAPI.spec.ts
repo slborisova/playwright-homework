de6df78e6d5cf0d7f5ac7955154599cc16e16d9d
slborisova <slborisova@gmail.com>
2025-05-19 20:02:38 -0400

    mocking_API-reguest_created

---
+++ tests/workingWithAPI.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from "@playwright/test";
+import owners from "../test-data/owners.json";
+
+test.beforeEach(async ({ page }) => {
+  await page.route(
+    "https://petclinic-api.bondaracademy.com/petclinic/api/owners",
+    async (route) => {
+      await route.fulfill({ body: JSON.stringify(owners) });
+    }
+  );
+
+  await page.route(
+    "https://petclinic-api.bondaracademy.com/petclinic/api/owners/289",
+    async (route) => {
+      await route.fulfill({
+        body: JSON.stringify(owners[0]),
+      });
+    }
+  );
+
+  await page.goto("/");
+  await page.getByText("Owners").click();
+  await page.getByText("Search").click();
+  await page.waitForLoadState("networkidle");
+});
+
+test("mocking API request", async ({ page }) => {
+  await expect(page.locator(".ownerFullName")).toHaveCount(2);
+
+  const ownerRow = page.getByText("Miranda Day");
+  await ownerRow.click();
+  await page.waitForLoadState("networkidle");
+  
+  await expect(page.getByRole("heading").first()).toHaveText("Owner Information");
+
+  await expect(page.locator(".ownerFullName")).toHaveText("Miranda Day");
+
+  await expect(page.locator("app-pet-list")).toHaveCount(2);
+
+  await expect(page.locator("app-pet-list").first()).toContainText("Buddy");
+  await expect(page.locator("app-pet-list").last()).toContainText("Bella");
+
+  await expect(page.locator("table.table-condensed").first().getByRole("button", { name: "Edit Visit" })).toHaveCount(10);
+});
