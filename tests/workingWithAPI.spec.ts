4021205e2fefc30180420fae69c6fc32bc6be6a0
slborisova <141890054+slborisova@users.noreply.github.com>
2025-06-03 19:08:28 -0400

    interceptAPIResponseHomework (#9)
    * interceptAPIResponseHomework
    
    * interceptAPIResponseHomework fixed

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,41 +1,78 @@
 import { test, expect } from "@playwright/test";
 import owners from "../test-data/owners.json";
 
-test.beforeEach(async ({ page }) => {
-  await page.route(
-    "https://petclinic-api.bondaracademy.com/petclinic/api/owners",
-    async (route) => {
-      await route.fulfill({ body: JSON.stringify(owners) });
-    }
-  );
+test.describe("Owners", async () => {
+  test.beforeEach(async ({ page }) => {
+    await page.route("*/**/api/owners", async (route) => {
+        await route.fulfill({
+          body: JSON.stringify(owners) });
+      }
+    );
 
-  await page.route(
-    "https://petclinic-api.bondaracademy.com/petclinic/api/owners/289",
-    async (route) => {
-      await route.fulfill({
-        body: JSON.stringify(owners[0]),
-      });
-    }
-  );
+    await page.route("*/**/api/owners/289", async (route) => {
+        await route.fulfill({
+          body: JSON.stringify(owners[0]),
+        });
+      }
+    );
 
-  await page.goto("/");
-  await page.getByText("Owners").click();
-  await page.getByText("Search").click();
-  await expect(page.locator("h2")).toHaveText("Owners");
+    await page.goto("/");
+    await page.getByText("Owners").click();
+    await page.getByText("Search").click();
+    await expect(page.locator("h2")).toHaveText("Owners");
+  });
+
+  test("mocking API request", async ({ page }) => {
+    await expect(page.locator(".ownerFullName")).toHaveCount(2);
+
+    await page.getByText("Miranda Day").click();
+    await expect(page.getByRole("heading").first()).toHaveText("Owner Information");
+
+    await expect(page.locator(".ownerFullName")).toHaveText("Miranda Day");
+
+    await expect(page.locator("app-pet-list")).toHaveCount(2);
+
+    await expect(page.locator("app-pet-list").first()).toContainText("Buddy");
+    await expect(page.locator("app-pet-list").last()).toContainText("Bella");
+
+    await expect(page.locator("table.table-condensed").first().getByRole("button", { name: "Edit Visit" })).toHaveCount(10);
+  });
 });
 
-test("mocking API request", async ({ page }) => {
-  await expect(page.locator(".ownerFullName")).toHaveCount(2);
+test.describe("Veterinarians", async () => {
+  test.beforeEach(async ({ page }) => {
+    await page.route('*/**/api/vets', async (route) => {
+      const response = await route.fetch();
+      const responseBody = await response.json();
+      responseBody.forEach(vet => {
+          if (vet.firstName == "Sharon" && vet.lastName == "Jenkins"){
+                vet.specialties.push(
+                  { id: 10000, name: "ophthalmology" },
+                  { id: 10001, name: "oncology" },
+                  { id: 10002, name: "immunology" },
+                  { id: 10003, name: "toxicology" },
+                  { id: 10004, name: "cardiology" },
+                  { id: 10005, name: "neurology" },
+                  { id: 10006, name: "urology" },
+                  { id: 10007, name: "avian" },
+                  { id: 10008, name: "equine" },
+                  { id: 10009, name: "nutrition" }
+                );
+            }
+      })
 
-  await page.getByText("Miranda Day").click();
-  await expect(page.getByRole("heading").first()).toHaveText("Owner Information");
-  
-  await expect(page.locator(".ownerFullName")).toHaveText("Miranda Day");
+      await route.fulfill({
+        body: JSON.stringify(responseBody),
+      });
+    });
+  });
 
-  await expect(page.locator("app-pet-list")).toHaveCount(2);
+  test("intercept api response", async ({ page }) => {
+    await page.goto("/");
+    await page.getByText("Veterinarians").click();
+    await page.getByText("All").click();
+    await expect(page.locator("h2")).toHaveText("Veterinarians");
 
-  await expect(page.locator("app-pet-list").first()).toContainText("Buddy");
-  await expect(page.locator("app-pet-list").last()).toContainText("Bella");
-
-  await expect(page.locator("table.table-condensed").first().getByRole("button", { name: "Edit Visit" })).toHaveCount(10);
+    await expect(page.locator('tr').filter({ hasText: 'Sharon Jenkins' }).locator('td').nth(1)).toContainText("ophthalmology oncology immunology toxicology cardiology neurology urology avian equine nutrition");
+  });
 });
